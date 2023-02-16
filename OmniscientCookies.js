a5212ec5be0ed985be20e444d0fa57685537d7bc
Lunamoth78910 <118857545+Lunamoth78910@users.noreply.github.com>
2023-02-15 17:10:55 -0700

    Update OmniscientCookies.js

--- OmniscientCookies.js
+++ OmniscientCookies.js
@@ -1,2 +1,2 @@
 // Keeps the link the same
-Game.LoadMod("https://Lunamoth78910.github.io/OmniscientCookies/dist/main.js");
+Game.LoadMod("https://Lunamoth78910.github.io/OMNICOOKIES2/dist/main.js");
